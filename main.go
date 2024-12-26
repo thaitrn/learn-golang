7aa9d40dffd08ae29ecc0395df0c8c78b0fdff83
Huy, Dinh Van <huy.dinh@navigosgroup.com>
2024-12-26 10:44:45 +0700

    Create a func named result in Go

--- main.go
+++ main.go
@@ -6,15 +6,24 @@ import (
 )
 
 func main() {
+	fmt.Println("################# Welcome to Go Lang #################")
+
 	fmt.Println("Hello Word, this is my random number from 0 -> 100: ", rand.Intn(100))
+	fmt.Println("_________________________________________________________")
 
 	// sum
 	fmt.Println("Sum of 2 numb 102 and 22: ", sum(102, 22))
+	fmt.Println("_________________________________________________________")
 
 	// multiple result in go fnc
 	firtName, lastName := fullName("Van", "Huy")
 	fmt.Println("Print my name (Van Huy) with en version: ", lastName, firtName)
+	fmt.Println("_________________________________________________________")
 
+	// named return values
+	value := date(9,10,1997)
+	fmt.Println("date 9/10/1997: ", value)
+	fmt.Println("_________________________________________________________")
 }
 
 // func in Go
@@ -23,7 +32,13 @@ func sum(x, y int) int {
 }
 
 // Multiple results in fnc
-
 func fullName(firtName, lastName string) (string, string) {
 	return firtName, lastName
+}
+
+
+//  Named return values
+func date(d int, m int, y int) (date string) {
+	date = fmt.Sprintf("%2d/%d/%d", d, m, y)
+	return
 }
